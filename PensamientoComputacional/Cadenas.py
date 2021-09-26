aeade344ac166aeb919af307c458124b3b6083af
Kandiotti <KSCandiotti@gmail.com>
2021-09-26 12:36:45 -0500

    Siguiendo las Buenas Practicas - Cadenas

--- PensamientoComputacional/Cadenas.py
+++ PensamientoComputacional/Cadenas.py
@@ -1,4 +1,8 @@
-nombre = input('Escribe tu nombre: ')
-saludo = f'Hola, {nombre} esta cadena tiene '
-len(saludo)
-print(f'{saludo}{str(int(len(saludo))+13)} caracteres')
+def main():
+    nombre = input('Escribe tu nombre: ')
+    saludo = f'Hola, {nombre} esta cadena tiene '
+    print(f'{saludo}{str(int(len(saludo))+13)} caracteres')
+
+
+if __name__ == '__main__':
+    main()
