324a156da41976da02b6052dd2b0b3b29dd090e8
Kandiotti <KSCandiotti@gmail.com>
2021-09-26 22:48:16 -0500

    Scope Formato y Docstring V0.1

---
+++ PensamientoComputacional/docstring.py
@@ -0,0 +1,8 @@
+def func1(parametro):
+    """ La descripcion de lo que hace nuestra función
+    Parametro debe ser de tipo int, y puede ser cualquier entero
+    Regresa parametro + 5
+    """
+    return parametro + 5
+
+help(func1)
